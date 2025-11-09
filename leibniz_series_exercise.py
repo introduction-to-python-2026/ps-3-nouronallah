7286dabd063594c44e7dcd6b1fe75dd6ee9bc298
nouronallah <nour.onallah1234@gmail.com>
2025-11-09 05:31:32 -0800

    Fix indentation in Leibniz series calculation

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -4,5 +4,5 @@ def pi(n_terms):
   for i in range(n_terms):
    list_of_numbers.append((-1)**i/(2*i+1))
   sum_numbers = sum(list_of_numbers) 
-  pi = 4*(sum_numbers) 
+  pi = 4*(sum_numbers)
   return pi 
