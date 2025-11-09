2f4f950939d808ca444897186f2f479cc7aadabf
nouronallah <nour.onallah1234@gmail.com>
2025-11-09 05:24:38 -0800

    Implement Leibniz series to approximate pi

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,8 @@
-def approximate_pi(n_terms):
-    pass # replace pass with your code
+def pi(n_terms):
+
+  list_of_numbers = []
+  for i in range(n_terms):
+  list_of_numbers.append((-1)**i/(2*i+1))
+sum_numbers = sum(list_of_numbers) 
+pi = 4*(sum_numbers) 
+return pi 
