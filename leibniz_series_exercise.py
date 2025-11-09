c2bd693c94a6079cae1409a7bec79e4a773cd0f6
nouronallah <nour.onallah1234@gmail.com>
2025-11-09 05:52:30 -0800

    Refactor pi function to use leibniz_series list

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,7 +1,7 @@
 def pi(n_terms):
-  list_of_numbers = []
+  leibniz_series = []
   for i in range(n_terms):
-     list_of_numbers.append((-1)**i/(2*i+1))
-  sum_numbers = sum(list_of_numbers) 
+     leibniz_series.append((-1)**i/(2*i+1))
+  sum_numbers = sum(leibniz_series) 
   pi = 4*(sum_numbers)
   return pi 
