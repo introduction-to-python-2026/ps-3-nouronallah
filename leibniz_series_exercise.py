93f934811a24e5543181f8ff82e325445f2af076
nouronallah <nour.onallah1234@gmail.com>
2025-11-09 05:27:59 -0800

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -2,7 +2,7 @@ def pi(n_terms):
 
   list_of_numbers = []
   for i in range(n_terms):
-  list_of_numbers.append((-1)**i/(2*i+1))
-sum_numbers = sum(list_of_numbers) 
-pi = 4*(sum_numbers) 
-return pi 
+   list_of_numbers.append((-1)**i/(2*i+1))
+  sum_numbers = sum(list_of_numbers) 
+  pi = 4*(sum_numbers) 
+  return pi 
