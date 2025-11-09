eb4e80a33eaf889737ab7ac230ca80515762377e
nouronallah <nour.onallah1234@gmail.com>
2025-11-09 05:34:24 -0800

    Fix indentation in pi function

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,8 +1,7 @@
 def pi(n_terms):
-
   list_of_numbers = []
   for i in range(n_terms):
-   list_of_numbers.append((-1)**i/(2*i+1))
+     list_of_numbers.append((-1)**i/(2*i+1))
   sum_numbers = sum(list_of_numbers) 
   pi = 4*(sum_numbers)
   return pi 
